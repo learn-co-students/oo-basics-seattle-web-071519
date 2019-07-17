ce94ac387e6ac7d9dc55c60dfc0192c84cf46477
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-17 15:03:21 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title,guy = "", pages = 272, genre = "nonfiction")
+        @title = title
+        @author = guy
+        @page_count = pages
+        @genre
+       # binding.pry
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+boo = Book.new("My Book","Bob")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :color, :brand, :size, :material, :condition
+    @color = nil
+    @size = nil
+    @material = nil
+    @condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
