521a36592a530090b681098e9c2d092e354a7e66
ecargiohc <therealgracechoi@gmail.com>
2019-07-17 15:36:25 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+#has properties like a title, an author, a page count, etc. 
+#class is able to keep track of all of the genres of all of the Books we create
+#our books all have the ability to turn their own pages!
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+        # @author = author
+        # @page_count = page_count
+        # @genre = genre
+    end 
 
-
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        if self.condition == "tattered"
+            puts "The shoe has been repaired"
+        else
+            puts "Your shoe is as good as new!"
+            self.condition = "new"
+        end
+    end
+end
