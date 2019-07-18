3613797a5486c570bbd92f384ca16c73938247b7
Ellyn <eka249@nyu.edu>
2019-07-17 17:29:58 -0700

    commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_reader :title, :author, :genre, :page_count
+    
+    def initialize(title)
+        @title=title
+    end
+    ##  confused by this syntax
+    def author=(author)
+        @author= author
+    end
+
+    def page_count=(num)
+        @page_count=num
+    end
+
+    def genre=(genre)
+        @genre=genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
